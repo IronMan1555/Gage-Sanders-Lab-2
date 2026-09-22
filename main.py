0318b690e56fe9e1bd2ae29daeae4f603f5dfb71
Gage Sanders <gsanders29@setoncatholic.org>
2026-09-22 18:07:23 +0000

    Version 2

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-x = 20
-if x>20:
-    print("Unk")
-elif x<20:
-    print("Baby")
-else:
-    print("Who even are you?")
+import random
+game_number = random.randint(1,10)
+print(game_number)
+win = False
+while(win == False):
+    guess = int(input("Pick a random number 1-10: "))
+    if guess > game_number:
+        print("Too High")
+    elif guess < game_number:
+        print("Too Low")
+    else: 
+        print("YOU WIN!")
+        win = True
